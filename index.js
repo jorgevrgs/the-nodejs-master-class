33804378a1cc02e04276d67e1da84f862331763c
Jorge Vargas <jorgevargasla@gmail.com>
2021-01-12 12:41:12 -0500

    parsing headers

--- index.js
+++ index.js
@@ -22,18 +22,14 @@ const server = http.createServer(function (req, res) {
   // Get the HTTP method
   const method = req.method.toLowerCase();
 
+  //Get the headers as an object
+  const headers = req.headers;
+
   // Send the response
   res.end("Hello World!\n");
 
   // Log the request/response
-  console.log(
-    "Request received on path: " +
-      trimmedPath +
-      " with method: " +
-      method +
-      " and this query string: ",
-    queryStringObject
-  );
+  console.log("Request received with these headers: ", headers);
 });
 
 // Start the server

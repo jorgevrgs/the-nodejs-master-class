3e35e4ca50caf2d5fb3337295dd660f48114656f
Jorge Vargas <jorgevargasla@gmail.com>
2021-01-12 14:04:27 -0500

    adding https support

--- index.js
+++ index.js
@@ -4,13 +4,39 @@
  */
 
 // Dependencies
+const fs = require("fs");
 const http = require("http");
+const https = require("https");
 const url = require("url");
 const StringDecoder = require("string_decoder").StringDecoder;
 const config = require("./config");
 
-// Configure the server to respond to all requests with a string
-const server = http.createServer(function (req, res) {
+// Instantiate the HTTP server
+const httpServer = http.createServer(function (req, res) {
+  unifiedServer(req, res);
+});
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, function () {
+  console.log("The HTTP server is running on port " + config.httpPort);
+});
+
+// Instantiate the HTTPS server
+var httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  unifiedServer(req, res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+  console.log("The HTTPS server is running on port " + config.httpsPort);
+});
+
+// All the server logic for both the http and https server
+unifiedServer = function (req, res) {
   // Parse the url
   const parsedUrl = url.parse(req.url, true);
 
@@ -71,14 +97,7 @@ const server = http.createServer(function (req, res) {
       console.log("Returning this response: ", statusCode, payloadString);
     });
   });
-});
-
-// Start the server
-server.listen(config.port, function () {
-  console.log(
-    `The server is up and running on port ${config.port} in ${config.envName} mode.`
-  );
-});
+};
 
 // Define all the handlers
 let handlers = {};
